f9b6fe3c055fc508dea3dfda63acadf34b5898f2
zishan <zishanenator@gmail.com>
2025-03-01 12:17:29 +0530

    requirements added

---
+++ main.py
@@ -0,0 +1,5 @@
+from textSummarizer.logging import logger
+
+
+logger.info("Welcome to Text Summarizer Project")
+logger.info("This is a test log message")
--- template.py
+++ template.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-project_name = 'text_summarizer'
+project_name = 'textSummarizer'
 
 
 list_of_files = [
